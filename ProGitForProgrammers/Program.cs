580b2663e1fe46c47f5d47131c2f7ff435ceb1b3
swlincgu-crypto <swlincgu@gmail.com>
2026-06-27 18:14:13 +0800

    多了二行指令

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;char:
 
 namespace ProGitForProgrammers
 {
@@ -10,6 +10,8 @@ namespace ProGitForProgrammers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
         }
     }
 }
