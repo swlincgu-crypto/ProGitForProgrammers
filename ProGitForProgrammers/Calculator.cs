58399390b31ff542ab5d8b86d9972c2afb2995cc
swlincgu-crypto <swlincgu@gmail.com>
2026-06-30 21:19:28 +0800

    Add calcualtor class

---
+++ ProGitForProgrammers/Calculator.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ProGitForProgrammers
+{
+    class Calculator 
+    {
+        public int Add(int left, it right) 
+        {
+            return left + right;
+        }
+    }
+}
