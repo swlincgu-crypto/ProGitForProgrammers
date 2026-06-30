fc2dd05f0137b14dc045e7f61ee720e4f3c37730
swlincgu-crypto <swlincgu@gmail.com>
2026-06-30 21:25:37 +0800

    Add three properties

--- ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/Calculator.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProGitForProgrammers
 {
-    internal class Calculator
+    class Book
     {
+        public string Title { get; set }
+        public List<string> Authors { get; set; }
+        public DateTime PulicationDate { get; set; }
+
     }
 }
